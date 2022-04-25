cadcee501ba78fa8af7c0a657ba6593b3d91195b
Tyrone Ballesteros <tyrone.ballesteros@outlook.com>
2022-04-24 19:57:22 -0700

    snippets

--- CMS.UI.Models/Staff.cs
+++ CMS.UI.Models/Staff.cs
@@ -7,6 +7,23 @@ namespace CMS.UI.Models
 {
     public class Staff
     {
+        public string FirstName { get; set; }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        
+    }
+
+    class StaffDetails
+    {
+        
+    }
+
+    interface IStaffDetails
+    {
         
     }
 }
